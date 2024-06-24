05cc1572ebbb1ce69e6cc488e2eef91f29b08153
Дмитрий <dima.voron13666@gmail.com>
2024-06-24 21:25:23 +0300

    2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,22 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        System.out.println("Задание 1");
+
+        String firstName = "Ivan";
+        String lastName = "Ivanov";
+        String middleName = "Ivanovich";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("Ф. И. О. сотрудника — " + fullName);
+
+        System.out.println("Задание 2");
+
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + fullName.toUpperCase());
+
+        System.out.println("Задание 3");
+
+        fullName = "Иванов Семён Семёнович";
+        String fullNameTrue = fullName.replace("ё", "е");
+        System.out.println("Данные Ф. И. О. сотрудника — " + fullNameTrue);
     }
 }
